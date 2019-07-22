c903afb76f9c6b29bcf5f195c33ed90fb0b284da
ashish-keasofttech <51331831+ashish-keasofttech@users.noreply.github.com>
2019-07-22 12:33:38 +0530

    Add files via upload

---
+++ includes/database.php
@@ -0,0 +1,16 @@
+<?php
+// core files
+include_once('config.php');
+include_once('MysqliDb.php');
+
+// specify your own database credentials
+$host = hostname;
+$dbname = dbname;
+$username = username;
+$password = password;
+
+// mysql wrapper
+$mysqli = new mysqli ($host, $username, $password, $dbname);
+$db = new MysqliDb ($mysqli);
+
+?>
